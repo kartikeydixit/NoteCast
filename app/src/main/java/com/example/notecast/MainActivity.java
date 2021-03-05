f5884de03eab74842b1547b0953bdc51d7de7aae
kartikeydixit <kartikeydixit1401@gmail.com>
2021-03-05 16:49:29 +0530

    this is first commit

---
+++ app/src/main/java/com/example/notecast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.notecast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void btnClick(View view){
+        Toast.makeText(this, "the button is clicked now audio will be recorded", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
